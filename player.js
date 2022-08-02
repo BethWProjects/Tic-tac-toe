9bbe65e0e127d92e2fe5ada2fa8f839e391920a7
Beth Wilson <bethwilson777@gmail.com>
2022-08-02 15:00:29 -0600

    Initial commit

---
+++ player.js
@@ -0,0 +1,9 @@
+class Player {
+  constructor(){
+
+  }
+}
+
+// Player methods must include, but are not limited to:
+// constructor - properties should include: id (ex: 'one'), token (ex: '⭐️'), wins (ex: 0)
+// increaseWins - increases the count of the player’s wins
