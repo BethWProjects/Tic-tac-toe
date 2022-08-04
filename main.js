aec034ee0c0c6647a1b048b40a5d5044123a729f
Beth Wilson <bethwilson777@gmail.com>
2022-08-04 10:24:18 -0600

    Add pseucoding and beginning of playGame function

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
+ //file that contains all DOM related JavaScript
+
 //query selectors
 
 //data
 
 var winningNumbers = [
-  
+
 ]
 
- //file that contains all DOM related JavaScript
+//event listeners
+//event listener for playGame function on click
+
+//function to update the dom using using innerHTML
+// function playGame(){
+// //innderHTML code to add image on click
+// `<table class="game-box">
+//   <tbody>
+//   <tr class="squares">
+//     <td class="square-1"></td>
+//     <td class="square-2">${player.id or player.token}</td>
+//     <td class="square-3"></td>
+//   </tr>
+//   <tr>
+//     <td class="square-4"></td>
+//     <td class="square-5"></td>
+//     <td class="square-6"></td>
+//   </tr>
+//   <tr>
+//     <td class="square-7"></td>
+//     <td class="square-8"></td>
+//     <td class="square-9"></td>
+//   </tr>
+//   </tbody>
+// </table>`
+// }
