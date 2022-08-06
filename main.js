1f42a885ef5381c9c0d271eb0ededfcad378e601
Beth Wilson <bethwilson777@gmail.com>
2022-08-06 14:25:16 -0600

    Add updateTurn function and modified event listeners and playGame function.  Instantiated player and game classes

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
  //file that contains all DOM related JavaScript
+//instantiated classes
+var game = new Game()
+var player = new Player()
 
 //query selectors
 var tiles = document.getElementById('tiles')
@@ -8,32 +11,22 @@ var tiles = document.getElementById('tiles')
 //event listeners
 //event listener for playGame function on click
 tiles.addEventListener('click', playGame);
-
+tiles.addEventListener('click', updateTurn);
 //functions
-function playGame(){
-    console.log('hello')
-}
-
-//function to update the dom using using innerHTML
 // function playGame(){
-// //innderHTML code to add image on click
-// `<table class="game-box">
-//   <tbody>
-//   <tr class="squares">
-//     <td class="square-1"></td>
-//     <td class="square-2">${player.id or player.token}</td>
-//     <td class="square-3"></td>
-//   </tr>
-//   <tr>
-//     <td class="square-4"></td>
-//     <td class="square-5"></td>
-//     <td class="square-6"></td>
-//   </tr>
-//   <tr>
-//     <td class="square-7"></td>
-//     <td class="square-8"></td>
-//     <td class="square-9"></td>
-//   </tr>
-//   </tbody>
-// </table>`
+//   console.log('hello')
+//
 // }
+function updateTurn(){
+  game.playersTurn()
+  if (game.turn === 1) {
+    tiles.innerHTML += `<td class="square-1">${player1.token}</td>`
+  } else if (game.turn === 2) {
+    tiles.innerHTML += `<td class="square-1">${player2.token}</td>`
+  }
+  console.log("update tiles");
+}
+// function to update the dom using using innerHTML
+function playGame(){
+  console.log("playGame on click")
+}
