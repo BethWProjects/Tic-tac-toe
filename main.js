1d869f69999e0153f24c446243c3514fecc3e4b8
Beth Wilson <bethwilson777@gmail.com>
2022-08-07 12:50:19 -0600

    Add event delegation function, event listener and query selector for targeting cells in the table

--- main.js
+++ main.js
@@ -4,32 +4,54 @@ var game = new Game()
 var player = new Player()
 
 //query selectors
-var tiles = document.getElementById('tiles')
-
-//data
+/*  create a query Selector for the entire table body(parent node)
+to use event delegation to access the individual table squares
+*/
+var tbody = document.querySelector('#tiles');
+/*  created event listener and handler called getTarget function
+to access each square and "set it to cell" */
+tbody.addEventListener('click', getTarget);
 
 //event listeners
 //event listener for playGame function on click
 tiles.addEventListener('click', playGame);
 tiles.addEventListener('click', updateTurn);
+
 //functions
-// function playGame(){
-//   console.log('hello')
-//
-// }
+function getTarget(e){
+  /* this function uses event delegation to target the individual cells
+  in the table, calling on the parent node */
+  var cell = e.target.closest('td');
+  if (!cell) {return;} // Quit, not clicked on a cell
+  var row = cell.parentElement;
+  console.log(cell.innerHTML, row.rowIndex, cell.cellIndex);
+// adding the players token to the cell that was clicked
+  if (game.turn === 0) {
+    cell.innerHTML += ''
+  } else if (game.turn === 1) {
+    cell.innerHTML += `<td class="square-1">${player1.token}</td>`
+    // player.updateBoxes(game.turn, data)
+  } else if (game.turn === 2) {
+    cell.innerHTML += `<td class="square-1">${player2.token}</td>`
+    // player.updateBoxes(game.turn, data)
+  }
+
+}
+
+
 function updateTurn(){
   var boxId;
   game.playersTurn()
   game.updateBoxes(boxId)
   game.checkWinner()
-  if (game.turn === 1) {
-    tiles.innerHTML += `<td class="square-1">${player1.token}</td>`
-    // player.updateBoxes(game.turn, data)
-  } else if (game.turn === 2) {
-    tiles.innerHTML += `<td class="square-1">${player2.token}</td>`
-    // player.updateBoxes(game.turn, data)
-  }
-  console.log("update tiles");
+} if (game.turn === 1) {
+  cell.innerHTML += `<td class="square-1">${player1.token}</td>`
+  // player.updateBoxes(game.turn, data)
+} else if (game.turn === 2) {
+  cell.innerHTML += `<td class="square-1">${player2.token}</td>`
+  // player.updateBoxes(game.turn, data)
+}
+
 }
 // function to update the dom using using innerHTML
 function playGame(){
