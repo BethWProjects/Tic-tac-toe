0902cf93a68e7134fae72af3f1fd03cb8b2fa7ef
Beth Wilson <bethwilson777@gmail.com>
2022-08-07 17:55:49 -0600

    Clean up code and update getTarget function

--- main.js
+++ main.js
@@ -3,31 +3,27 @@
 var game = new Game()
 var player = new Player()
 
-//query selectors with matched event listener
-/*  create a query Selector for the entire table body(parent node)
+//query selectors with matched event listeners
+/* -created a query Selector for the entire table body(parent node)
 to use event delegation to access the individual table squares
-*/
-var tbody = document.querySelector('#tiles');
-/*  created event listener and handler called getTarget function
+- created event listener and handler called getTarget function
 to access each square and "set it to cell" */
+var tbody = document.querySelector('#tiles');
 tbody.addEventListener('click', getTarget);
 
-// var playerTurnTitle = document.querySelector('#player-turn')
-// playerTurnTitle.addEventListener('click', updateTurn)
-tiles.addEventListener('click', updateTurn)
-//event listeners
-//event listener for playGame function on click
-// tiles.addEventListener('click', playGame);
 
 //functions
 function getTarget(e){
+  //invoking game functions to sync data model
+  game.playersTurn()
+
+  /* getTarget function uses event delegation to target the individual cells
+  in the table, calling on the parent node.  When invoked in the event listener
+  the innerHTML and innerText will update on click. */
   var playerTurnTitle = document.querySelector('#player-turn')
-  /* this function uses event delegation to target the individual cells
-  in the table, calling on the parent node */
   var cell = e.target.closest('td');
   if (!cell) {return;} // Quit, not clicked on a cell
-  var row = cell.parentElement;
-  console.log(cell.innerHTML, row.rowIndex, cell.cellIndex);
+
 // adding the players token to the cell that was clicked
 if (game.turn === 1) {
     cell.innerHTML += `<td class="square-1">${player1.token}</td>`
@@ -38,25 +34,14 @@ if (game.turn === 1) {
     playerTurnTitle.innerText = `It's ${player1.token} 's turn`
     // player.updateBoxes(game.turn, data)
   }
-
-}
-
-function updateTurn(){
-  var boxId;
-  game.playersTurn()
+  /* grab the cell.id (id from the 'td' element) and create a variable to
+  use as a parameter in updateBoxes, to push the id number into the boxes array
+  for the player who's turn it is. */
+  var boxId = cell.id
+  console.log(cell.id);
   game.updateBoxes(boxId)
+
+  // Need to check winner after the player's "boxes" is updated
   game.checkWinner()
 
-//  if (game.turn === 1) {
-//   playerTurnTitle.innerHTML += `<p id="player-turn" class="main-title">It's ${player1.token} turn</p>`
-//   // player.updateBoxes(game.turn, data)
-// } else if (game.turn === 2) {
-//   playerTurnTitle.innerHTML += `<p id="player-turn" class="main-title">It's ${player2.token} turn</p>`
-//   // player.updateBoxes(game.turn, data)
-// }
-}
-
-// function to update the dom using using innerHTML
-function playGame(){
-  console.log("playGame on click")
 }
