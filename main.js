6185a85136616e195a159bc55a1c8929e5aa2dbe
Beth Wilson <bethwilson777@gmail.com>
2022-08-08 13:48:08 -0600

    Update clearBoard logic

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
  //file that contains all DOM related JavaScript
 //instantiated classes
-var game = new Game()
+var game = new Game(player1, player2)
 var player = new Player()
 
 //query selectors with matched event listeners
@@ -23,10 +23,16 @@ function getTarget(e){
   var playerTurnTitle = document.querySelector('#player-turn')
   var cell = e.target.closest('td');
   if (!cell) {return;} // Quit, not clicked on a cell
+  // const row = cell.parentElement;
+  // var player1Row = [0, 0, 0]
+  // var player1Column = ["", "", ""]
 
 // adding the players token to the cell that was clicked
 if (game.turn === 1) {
     cell.innerHTML += `<td class="square-1">${player1.token}</td>`
+    // var x = row.rowIndex
+    // player1.boxes[x]+=1
+    // player1Column[cell.cellIndex]+=1
     playerTurnTitle.innerText = `It's ${player2.token} 's turn`
     // player.updateBoxes(game.turn, data)
   } else if (game.turn === 2) {
@@ -37,11 +43,23 @@ if (game.turn === 1) {
   /* grab the cell.id (id from the 'td' element) and create a variable to
   use as a parameter in updateBoxes, to push the id number into the boxes array
   for the player who's turn it is. */
-  var boxId = cell.id
-  console.log(cell.id);
+  var boxId = cell.id  //Maybe I need to change to divs?
+  console.log('cell index', cell.id);
   game.updateBoxes(boxId)
+
+  // var boxId = cell.cellIndex  //Maybe I need to change to divs?
+  // console.log('cell index', cell.cellIndex);
+  // game.updateBoxes(boxId)
 
   // Need to check winner after the player's "boxes" is updated
   game.checkWinner()
 
 }
+//
+// function clearBoard(){
+//   if (tbody.innerText === `${player1.token}` || tbody.innerText === `${player2.token}`){
+//     return false;
+//   }
+//   return true;
+// }
+// clearBoard()
