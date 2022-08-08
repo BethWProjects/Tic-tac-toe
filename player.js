d10853cdae69e77399bb78d3013b908f95f8e5c6
Beth Wilson <bethwilson777@gmail.com>
2022-08-08 17:26:22 -0600

    Remove instantiation and placed in main.js

--- player.js
+++ player.js
@@ -14,10 +14,10 @@ class Player {
   //   }
   // }
 }
-var player1 = new Player(1, "🦄")
-var player2 = new Player(2, "🌈")
-console.log(player1);
-console.log(player2);
+// var player1 = new Player(1, "🦄")
+// var player2 = new Player(2, "🌈")
+// console.log(player1);
+// console.log(player2);
 // Player methods must include, but are not limited to:
 // constructor - properties should include: id (ex: 'one'), token (ex: '⭐️'), wins (ex: 0)
 // increaseWins - increases the count of the player’s wins
